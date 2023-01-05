f764135d68614aa0963c286e8f1c0abe91edc552
Ana <ana.alves@inkotecnologia.com.br>
2023-01-05 10:50:45 -0300

    some for loop exercices

---
+++ javascript/palindromos/index.js
@@ -0,0 +1,11 @@
+const palavra = prompt("informe uma palavra")
+let palavraInvertida = ""
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+    palavraInvertida += palavra[i]
+    }
+    if (palavra === palavraInvertida) {
+    alert(palavra + " é um palíndromo!")
+    } else {
+    alert(palavra + " não é um palíndromo!\n\n" + palavra + " !== " + palavraInvertida)
+    }
